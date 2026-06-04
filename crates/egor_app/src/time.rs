38fb68730381a2e7738bf1cda162ac8f8e628e73
Jonatino <jonathanbeaudoin1996@gmail.com>
2026-06-04 00:40:33 -0400

    fixed bug with egor frame time calc after long uptime causing it to infinite busy loop

--- crates/egor_app/src/time.rs
+++ crates/egor_app/src/time.rs
@@ -2,19 +2,15 @@
 use std::time::Instant;
 
 #[cfg(target_arch = "wasm32")]
-fn now() -> f32 {
-    (web_sys::window().unwrap().performance().unwrap().now() / 1000.0) as f32
-}
-
-#[cfg(not(target_arch = "wasm32"))]
-fn now(start: Instant) -> f32 {
-    start.elapsed().as_secs_f32()
+fn now() -> f64 {
+    web_sys::window().unwrap().performance().unwrap().now() / 1000.0
 }
 
 pub struct FrameTimer {
     #[cfg(not(target_arch = "wasm32"))]
-    start: Instant,
-    last_time: f32,
+    last_frame_at: Instant,
+    #[cfg(target_arch = "wasm32")]
+    last_frame_at: f64,
     accumulator: f32,
     frame_count: u32,
     /// Time in seconds since the last frame
@@ -29,8 +25,9 @@ impl Default for FrameTimer {
     fn default() -> Self {
         Self {
             #[cfg(not(target_arch = "wasm32"))]
-            start: Instant::now(),
-            last_time: 0.0,
+            last_frame_at: Instant::now(),
+            #[cfg(target_arch = "wasm32")]
+            last_frame_at: now(),
             accumulator: 0.0,
             frame_count: 0,
             delta: 0.0,
@@ -43,19 +40,19 @@ impl Default for FrameTimer {
 impl FrameTimer {
     /// Updates delta time & calculates FPS
     pub(crate) fn update(&mut self) {
-        let cur_time = {
-            #[cfg(not(target_arch = "wasm32"))]
-            {
-                now(self.start)
-            }
-            #[cfg(target_arch = "wasm32")]
-            {
-                now()
-            }
-        };
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            let now = Instant::now();
+            self.delta = now.saturating_duration_since(self.last_frame_at).as_secs_f32();
+            self.last_frame_at = now;
+        }
 
-        self.delta = cur_time - self.last_time;
-        self.last_time = cur_time;
+        #[cfg(target_arch = "wasm32")]
+        {
+            let now = now();
+            self.delta = (now - self.last_frame_at).max(0.0) as f32;
+            self.last_frame_at = now;
+        };
 
         self.accumulator += self.delta;
         self.frame_count += 1;
